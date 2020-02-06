3bf7220f31d457b62ff0111f72237d20ca782b6e
justass490 <55572439+justass490@users.noreply.github.com>
2020-02-06 11:21:43 +0200

    Add files via upload

---
+++ untitled/src/sample/Controller.java
@@ -0,0 +1,51 @@
+package sample;
+
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    public void press1(ActionEvent event){
+        System.out.println("1");
+    }
+    public void press2(ActionEvent event){
+        System.out.println("2");
+    }
+    public void press3(ActionEvent event){
+        System.out.println("3");
+    }
+    public void press4(ActionEvent event){
+        System.out.println("4");
+    }
+    public void press5(ActionEvent event){
+        System.out.println("5");
+    }
+    public void press6(ActionEvent event){
+        System.out.println("6");
+    }
+    public void press7(ActionEvent event){
+        System.out.println("7");
+    }
+    public void press8(ActionEvent event){
+        System.out.println("8");
+    }
+    public void press9(ActionEvent event){
+        System.out.println("9");
+    }
+    public void press0(ActionEvent event){
+        System.out.println("0");
+    }
+    public void pressDot(ActionEvent event){
+        System.out.println(".");
+    }
+    public void pressMinus(ActionEvent event){
+        System.out.println("-");
+    }
+    public void pressPlus(ActionEvent event){
+        System.out.println("+");
+    }
+    public void pressDivision(ActionEvent event){
+        System.out.println("/");
+    }
+    public void pressMultiplication(ActionEvent event){
+        System.out.println("*");
+    }
+}
